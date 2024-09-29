25fa2b82efdcc2086cb4887c5095c8dc0114697d
diegop19 <daraya.0906@estudiantec.cr>
2024-09-28 20:36:26 -0600

    interfaz de generar solucion

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 "Proyecto 01 Laberinto // Curso de Análisis de Algoritmos // Diego y Raúl"
 "Página principal"
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('menu_inicio.html')
+
+@app.route('/generar_solucion')
+def generar_solucion():
+    return render_template('generar_solucion.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
