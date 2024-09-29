8e7684667f36f85150818ef88aed3541922a2160
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-09-28 20:41:33 -0600

    ñaña

--- app.py
+++ app.py
@@ -10,15 +10,13 @@ app = Flask(__name__)
 def index():
     return render_template('menu_inicio.html')
 
-<<<<<<< HEAD
 @app.route('/generar_solucion')
 def generar_solucion():
     return render_template('generar_solucion.html')
-=======
+
 @app.route('/crear_laberinto')
-def crear_laberinto():
+def generar_solucion():
     return render_template('crear_laberinto.html')
->>>>>>> 7b17f28dc01b6196a175cf928356c859357a415c
 
 if __name__ == '__main__':
     app.run(debug=False)
