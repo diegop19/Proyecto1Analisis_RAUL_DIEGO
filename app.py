7b17f28dc01b6196a175cf928356c859357a415c
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-09-28 20:34:35 -0600

    Interfaz de Creación del laberinto

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 "Proyecto 01 Laberinto // Curso de Análisis de Algoritmos // Diego y Raúl"
 "Página principal"
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Ruta para el menú principal
 @app.route('/')
 def index():
     return render_template('menu_inicio.html')
+
+@app.route('/crear_laberinto')
+def crear_laberinto():
+    return render_template('crear_laberinto.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
