b11d6b4d76590d9c6a2889d82cac67a94f3e13be
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-10-03 22:31:10 -0600

    Creación de matriz solución

---
+++ construir_matriz_solucion.py
@@ -0,0 +1,8 @@
+"Algoritmo para modificar la matriz y construir la matriz con la solución dada por la lista de tuplas"
+
+def construir_solucion(matriz, coordenadas):
+    for (i, j) in coordenadas:
+        matriz[i][j] = 3
+    
+    print("Identificar matriz solucion: ", matriz)
+    return matriz
